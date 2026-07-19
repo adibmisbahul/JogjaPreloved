26eccc3de4a4ec0185ce6dcd923e03d77085c12d
adibmisbahul <adibmisbahul101812@gmail.com>
2026-07-19 10:32:08 -0700

    card dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,16 +5,40 @@ import Card from "../components/card";
 import { AiFillProduct } from "react-icons/ai";
 
 const Dashboard = () => {
-  const [sale , setSale] = useState(false)
+  const [sale, setSale] = useState(false);
+
+  const jualProduct = () => {
+    alert("click");
+  };
+
   return (
     <div className="dashboard">
-      <main>
+      <header className="header-dashboard">
         <div className="main-card">
-      <CardDashboard icon={<AiFillProduct/>} data={"5"} desc={"total barang yang anda tawarkan"}/> 
-      <CardDashboard icon={<AiFillProduct/>} data={"5"} desc={"total barang yang anda tawarkan"}/> 
-      <CardDashboard icon={<AiFillProduct/>} data={"5"} desc={"total barang yang anda tawarkan"}/> 
-      <CardDashboard icon={<AiFillProduct/>} data={"5"} desc={"total barang yang anda tawarkan"}/> 
+          <CardDashboard
+            icon={<AiFillProduct />}
+            data={"15"}
+            desc={"total barang yang anda tawarkan"}
+          />
+          <CardDashboard
+            icon={<AiFillProduct />}
+            data={"10"}
+            desc={"barang terjual"}
+          />
+          <CardDashboard
+            icon={<AiFillProduct />}
+            data={"5"}
+            desc={"barang tersedia"}
+          />
+          <CardDashboard
+            icon={<AiFillProduct />}
+            data={"15.000.000"}
+            desc={"total pendapatan"}
+          />
         </div>
+      </header>
+      <main className="main-dashboard">
+        <button onClick={jualProduct}>jual</button>
       </main>
     </div>
   );
