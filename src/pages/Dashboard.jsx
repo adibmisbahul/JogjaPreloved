8e64dfa9265c059b383fb7fe1b767469abf46664
adibmisbahul <adibmisbahul101812@gmail.com>
2026-07-23 06:30:03 -0700

    setup ag-grid-react

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import CardDashboard from "../components/cardDashboard";
 import Card from "../components/card";
 import { AiFillProduct } from "react-icons/ai";
+import { AllCommunityModule } from "ag-grid-community";
+import { AgGridProvider } from "ag-grid-react";
+import { AgGridReact } from "ag-grid-react";
 
 const Dashboard = () => {
   const [sale, setSale] = useState(false);
@@ -10,6 +13,22 @@ const Dashboard = () => {
   const jualProduct = () => {
     alert("click");
   };
+  const modules = [AllCommunityModule];
+
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
 
   return (
     <div className="dashboard">
@@ -39,6 +58,17 @@ const Dashboard = () => {
       </header>
       <main className="main-dashboard">
         <button onClick={jualProduct}>jual</button>
+        {sale
+          ? () => {
+              return;
+            }
+          : () => {}}
+        <AgGridProvider modules={modules}>
+          {/* Data Grid will fill the size of the parent container */}
+          <div style={{ height: 500 }}>
+            <AgGridReact rowData={rowData} columnDefs={colDefs} />
+          </div>
+        </AgGridProvider>
       </main>
     </div>
   );
